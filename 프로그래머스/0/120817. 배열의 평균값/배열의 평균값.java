ea4163a446a17f17e22dcd41b6f967bf95dcc212
jinju <103505641+jinju0323@users.noreply.github.com>
2025-01-08 21:42:28 +0900

    Update 배열의 평균값.java
    - 주석 추가

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -1,10 +1,10 @@
 class Solution {
     public double solution(int[] numbers) {
-        double result = 0;
+        double result = 0; // 누적합산할 변수를 0으로 초기화
         
         for (int i = 0; i < numbers.length; i++) {
-            result += numbers[i];
+            result += numbers[i]; // i번째 원소를 누적합산
         }
-        return result / numbers.length;
+        return result / numbers.length; // 배열의 길이만큼 나누어 평균 계산
     }
 }
